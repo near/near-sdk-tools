2024017ae4822f3a76e575a092712e6a6cfd0f7d
Willem Wyndham <willem@cs.umd.edu>
2020-05-01 11:28:27 -0400

    Binary tries to install itself if not already installed

--- npm/run.js
+++ npm/run.js
@@ -1,9 +1,25 @@
 #!/usr/bin/env node
 
 const getBinary = require('./getBinary');
+const join = require("path").join;
+const fs = require("fs");
+
+const binary = getBinary();
+const binPath = join(binary.installDirectory, "bin", binary.name);
+
+function binExists() {
+  return fs.existsSync(binPath);
+}
+
+if (!binExists()) {
+  binary.install();
+  if (!binExists()) {
+    throw new Error("binary failed to be installed to " + binPath);
+  }
+}
 
 if (process.argv.length < 3) {
   process.argv.push("--help");
 }
 
-getBinary().run();
+binary.run();
